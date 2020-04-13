7d0f06fc9ad86fe4ec724b21af9bbcd3717c119f
Spydneo <jairoecalderon@gmail.com>
2020-04-13 19:52:25 +0100

    Cl_73: Acción de guardar artículos y validación de datos

--- controllers/article.js
+++ controllers/article.js
@@ -1,14 +1,58 @@
 'use strict'
 
+var validator = require('validator');
+var Article = require('../models/article');
+
 var controller = {
 
     datosCurso: (req, response) => {
         var name = req.body.nombre;
-        return response.status(200).send({ message: 'hola ' + name });
+        return response.status(200).send({
+            message: 'hola ' + name
+        });
     },
 
     test: (req, res) => {
-        return res.status(200).send({ message: 'Mensaje de ruta Test' });
+        return res.status(200).send({
+            message: 'Mensaje de ruta TEST'
+        });
+    },
+
+    save: (req, res) => {
+        //Recoger los parametros por POST
+        var params = req.body;
+        // console.log("Params" + params); //No funciona
+
+        //Validar datos
+        try {
+            var validate_title = !validator.isEmpty(params.title); //Cuando no este vacio el título
+            var validate_content = !validator.isEmpty(params.content); //Cuando no este vacio el content
+
+        } catch (error) {
+            return res.status(400).send({
+                message: 'Faltan datos por enviar'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            //Crear el objeto a guardar
+
+            //Asignar valores
+
+            //Guardar el artículo
+
+            //Devolver una respuesta
+            return res.status(200).send({
+                article: params
+            });
+
+        } else {
+            return res.status(400).send({
+                message: 'Los datos no son validos'
+            });
+        }
+
+
     }
 
 };
--- routes/article.js
+++ routes/article.js
@@ -5,8 +5,10 @@ var ArticleController = require('../controllers/article');
 
 var router = express.Router();
 
+//Rutas de prueba
 router.post('/datos-curso', ArticleController.datosCurso);
 router.get('/test-controlador', ArticleController.test);
-
+//Rutas útilies
+router.post('/save', ArticleController.save);
 
 module.exports = router;
