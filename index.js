80f44c908a3aa4d2fa0af58fef95d876e53b9134
Spydneo <jairoecalderon@gmail.com>
2021-03-16 19:05:56 +0000

    change test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ mongoose.set('useFindAndModify', false); // Los métodos antiguos desactivados
 mongoose.Promise = global.Promise; //Configuración interna
 mongoose.connect('mongodb://localhost:27017/api_rest_blog', { useNewUrlParser: true })
     .then(() => {
-        console.log("Conexión con DB correcta!!");
+        console.log("Conexión con DB correcta!!+1");
         //Crear servidor y ponerme a escuchar peticiones HTTP
         app.listen(port, () => {
             console.log('Servidor corriendo en http://localhost:' + port);
